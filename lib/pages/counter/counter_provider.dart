315c3c418b095ac2729ad093d6349276864be734
Reza Safavi <rezasfv36@gmail.com>
2025-02-19 16:33:27 +0100

    use code generation

--- lib/pages/counter/counter_provider.dart
+++ lib/pages/counter/counter_provider.dart
@@ -1,19 +1,37 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class Counter extends FamilyNotifier<int, int> {
+part 'counter_provider.g.dart';
+
+// class Counter extends FamilyNotifier<int, int> {
+//   @override
+//   int build(int arg) {
+//     ref.onDispose(() {
+//       print('[CounterProvider] Disposed');
+//     });
+//     return arg;
+//   }
+
+//   void increment() {
+//     state++;
+//   }
+// }
+
+// final counterProvider = NotifierProvider.family<Counter, int, int>(() {
+//   return Counter();
+// });
+
+@riverpod
+class Counter extends _$Counter {
   @override
-  int build(int arg) {
+  int build(int initialValue) {
     ref.onDispose(() {
       print('[CounterProvider] Disposed');
     });
-    return arg;
+    return initialValue;
   }
 
   void increment() {
     state++;
   }
 }
-
-final counterProvider = NotifierProvider.family<Counter, int, int>(() {
-  return Counter();
-});
